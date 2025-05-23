139f2ac928e4d9d34bf305fc7f746b50a5caf3f2
farici0og <fabricio.camposprofissional@gmail.com>
2025-05-23 10:30:04 -0300

    Project Created

---
+++ src/main/java/com/dslist/intensivo/IntensivoApplication.java
@@ -0,0 +1,13 @@
+package com.dslist.intensivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivoApplication.class, args);
+	}
+
+}
